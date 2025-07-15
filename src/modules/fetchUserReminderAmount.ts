a1ce934c781360200e37b0bacabc2208ea008f55
Dave <dave.turmawan@outlook.com>
2025-07-15 21:09:29 +0700

    forgot check

--- src/modules/fetchUserReminderAmount.ts
+++ src/modules/fetchUserReminderAmount.ts
@@ -1,11 +1,18 @@
-import { remindersTable } from "../db/schema";
+import { remindersTable, notifiedTable } from "../db/schema";
 import db from "./db";
-import { count, eq } from "drizzle-orm";
+import { count, eq, and, notExists } from "drizzle-orm";
 
-export default async function fetchUserReminderAmount(userId: string): Promise<number> {
+export default async function fetchUserReminderAmount(provider: string, userId: string): Promise<number> {
     return await db.select({count: count()})
         .from(remindersTable)
         .where(
-            eq(remindersTable.ownerId, userId)
+            and(
+                eq(remindersTable.ownerId, `${provider}-${userId}`),
+                notExists(
+                    db.select()
+                        .from(notifiedTable)
+                        .where(eq(notifiedTable.reminderId, remindersTable.id))
+                )
+            )
         ).then(result => result[0]?.count || 0);
 }
