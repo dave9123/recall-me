3fd75d3300b030db77b5d6d7391876f1f9faa3dc
Dave <dave.turmawan@outlook.com>
2025-07-15 21:09:47 +0700

    clean up

--- src/test.ts
+++
@@ -1,6 +0,0 @@
-import "dotenv/config";
-import fetchUserReminderAmount from "./modules/fetchUserReminderAmount";
-
-fetchUserReminderAmount("slack-U07C3SJ99RP").then(amount => {
-    console.log(amount);
-});
